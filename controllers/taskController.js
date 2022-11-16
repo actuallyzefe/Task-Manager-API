eb8adf7af81896de94b8f530ae5bad70199121d0
actuallyzefe <zefek10@gmail.com>
2022-11-16 15:50:33 +0300

    updateTask

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -54,18 +54,12 @@ exports.createTask = async (req, res) => {
 // UPDATE
 exports.updateTask = async (req, res) => {
   try {
-    const updatedTask = await Task.findByIdAndUpdate(req.params.id);
+    // const { id: taskId } = req.params;
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body);
     res.status(200).json({
-      status: "SUCCESS",
-      message: "TASK UPDATED",
-      updatedTask: req.body,
+      status: "UPDATED",
+      task: task,
     });
-
-    if (!task) {
-      return res.status(404).json({
-        message: `NO TASK WITH THAT ID ${req.params.id}, (404)`,
-      });
-    }
   } catch (error) {
     res.status(500).json({
       message: error,
